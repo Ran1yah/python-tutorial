64ba4cdbc8ac8d6899e718d46d276c9e64e09c92
Raniyah Cole <s_raniyah.cole@ousd.org>
2021-06-29 13:22:00 -0500

    this is my lesson 2

---
+++ lesson2.py
@@ -0,0 +1,13 @@
+name = "raniyah"
+age = 15
+salary = 56.92
+plays_softball = True
+jersey_number = "38"
+
+print(name)
+print(age, salary, plays_softball, jersey_number)
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_softball))
+print(type(jersey_number))
