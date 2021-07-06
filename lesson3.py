e8149fce2df5b0a4f1a3186195cae0bb4937da11
Raniyah Cole <s_raniyah.cole@ousd.org>
2021-07-06 14:22:23 -0500

    this is my lesson 3

---
+++ lesson3.py
@@ -0,0 +1,8 @@
+grocery_list{"chicken": 1.59, "Beef": 1.99, "Cheese": 1.00, "Milk":2.50}
+print
+NBA_players = {"Lebron James": 23, "Kevin Durant": 35, "Stephen Curry": 30, "Damian Lillard": 0}
+Steph_jersey = ["Stephen Curry"]
+NBA_players["Lebron James"] = 6
+NBA_players["Lebron James"] -= 17
+print
+shoe_names{"Jordan 13": 1, "Yeezy": 8, "Foamposite": 10, "Air Max": 5, "SB Dunk": 20}
